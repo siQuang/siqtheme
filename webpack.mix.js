da2b2ed3b70ad42e4728ffeecd0697bac4abf36f
Simon Nguyen <simonquang@gmail.com>
2020-04-26 02:07:22 -0700

    Minor change to package.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,6 @@ const pages = require('./pages');
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for your application, as well as bundling up your JS files.
- |
  */
 mix.sass('src/assets/sass/siqtheme.scss', 'assets/css/')
     .js('src/assets/scripts/siqtheme.js', 'assets/scripts/')
