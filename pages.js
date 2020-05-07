10daa2d60c3484eb289da89987567bd595dd2362
Simon Nguyen <simonquang@gmail.com>
2020-05-07 03:05:08 -0700

    Reformat pages.js

--- pages.js
+++ pages.js
@@ -21,135 +21,135 @@ let themeName       = "siQtheme",
  */
 module.exports = [
     {
-        title: themeName + " by 3M Square",
-        template: sourcePath + "index.ejs",
-        filename: "index.html",
-        topmenu: dashboardMenu,
-        submenu: "dashboard1"
+        title:      themeName + " by 3M Square",
+        template:   sourcePath + "index.ejs",
+        filename:   "index.html",
+        topmenu:    dashboardMenu,
+        submenu:    "dashboard1"
     }, {
-        title: themeName + " - Date Time Picker",
-        template: sourcePath + "cp_datetime.ejs",
-        filename: "cp_datetime.html",
-        topmenu: componentsMenu,
-        submenu: "cpdatetime",
+        title:      themeName + " - Date Time Picker",
+        template:   sourcePath + componentsMenu + "_datetime.ejs",
+        filename:   componentsMenu + "_datetime.html",
+        topmenu:    componentsMenu,
+        submenu:    componentsMenu + "datetime",
     }, {
-        title: themeName + " - Bootstrap Toggle",
-        template: sourcePath + "cp_bstoggle.ejs",
-        filename: "cp_bstoggle.html",
-        topmenu: componentsMenu,
-        submenu: "cptoggle",
+        title:      themeName + " - Bootstrap Toggle",
+        template:   sourcePath + componentsMenu + "_bstoggle.ejs",
+        filename:   componentsMenu + "_bstoggle.html",
+        topmenu:    componentsMenu,
+        submenu:    componentsMenu + "toggle",
     }, {
-        title: themeName + " - UI Buttons",
-        template: sourcePath + "ui_buttons.ejs",
-        filename: "ui_buttons.html",
-        topmenu: uiElementsMenu,
-        submenu: "uibutton",
+        title:      themeName + " - UI Buttons",
+        template:   sourcePath + uiElementsMenu + "_buttons.ejs",
+        filename:   uiElementsMenu + "_buttons.html",
+        topmenu:    uiElementsMenu,
+        submenu:    uiElementsMenu + "button",
     }, {
-        title: themeName + " - UI Typography",
-        template: sourcePath + "ui_typography.ejs",
-        filename: "ui_typography.html",
-        topmenu: uiElementsMenu,
-        submenu: "uitypo"
+        title:      themeName + " - UI Typography",
+        template:   sourcePath + "ui_typography.ejs",
+        filename:   "ui_typography.html",
+        topmenu:    uiElementsMenu,
+        submenu:    "uitypo"
     }, {
-        title: themeName + " - UI Cards",
-        template: sourcePath + "ui_cards.ejs",
-        filename: "ui_cards.html",
-        topmenu: uiElementsMenu,
-        submenu: "uicard",
+        title:      themeName + " - UI Cards",
+        template:   sourcePath + uiElementsMenu + "_cards.ejs",
+        filename:   uiElementsMenu + "_cards.html",
+        topmenu:    uiElementsMenu,
+        submenu:    uiElementsMenu + "card",
     }, {
-        title: themeName + " - UI Colors",
-        template: sourcePath + "ui_colors.ejs",
-        filename: "ui_colors.html",
-        topmenu: uiElementsMenu,
-        submenu: "uicolor"
+        title:      themeName + " - UI Colors",
+        template:   sourcePath + uiElementsMenu + "_colors.ejs",
+        filename:   uiElementsMenu + "_colors.html",
+        topmenu:    uiElementsMenu,
+        submenu:    uiElementsMenu + "color"
     }, {
-        title: themeName + " - Fontawesome",
-        template: sourcePath + "ui_fontawesome.ejs",
-        filename: "ui_fontawesome.html",
-        topmenu: uiElementsMenu,
-        submenu: "uifa"
+        title:      themeName + " - Fontawesome",
+        template:   sourcePath + uiElementsMenu + "_fontawesome.ejs",
+        filename:   uiElementsMenu + "_fontawesome.html",
+        topmenu:    uiElementsMenu,
+        submenu:    uiElementsMenu + "fa"
     }, {
-        title: themeName + " - Themify Icons",
-        template: sourcePath + "ui_themify.ejs",
-        filename: "ui_themify.html",
-        topmenu: uiElementsMenu,
-        submenu: "uithemify"
+        title:      themeName + " - Themify Icons",
+        template:   sourcePath + uiElementsMenu + "_themify.ejs",
+        filename:   uiElementsMenu + "_themify.html",
+        topmenu:    uiElementsMenu,
+        submenu:    uiElementsMenu + "themify"
     }, {
-        title: themeName + " - Bootstrap Modals",
-        template: sourcePath + "ui_modals.ejs",
-        filename: "ui_modals.html",
-        topmenu: uiElementsMenu,
-        submenu: "uimodal"
+        title:      themeName + " - Bootstrap Modals",
+        template:   sourcePath + uiElementsMenu + "_modals.ejs",
+        filename:   uiElementsMenu + "_modals.html",
+        topmenu:    uiElementsMenu,
+        submenu:    uiElementsMenu + "modal"
     }, {
-        title: themeName + " - Toastr",
-        template: sourcePath + "ui_toastr.ejs",
-        filename: "ui_toastr.html",
-        topmenu: uiElementsMenu,
-        submenu: "uitoastr"
+        title:      themeName + " - Toastr",
+        template:   sourcePath + uiElementsMenu + "_toastr.ejs",
+        filename:   uiElementsMenu + "_toastr.html",
+        topmenu:    uiElementsMenu,
+        submenu:    uiElementsMenu + "toastr"
     }, {
-        title: themeName + " - Basic Tables",
-        template: sourcePath + "tb_basic.ejs",
-        filename: "tb_basic.html",
-        topmenu: tablesMenu,
-        submenu: "tbbasic"
+        title:      themeName + " - Basic Tables",
+        template:   sourcePath + tablesMenu + "_basic.ejs",
+        filename:   tablesMenu + "_basic.html",
+        topmenu:    tablesMenu,
+        submenu:    tablesMenu + "basic"
     }, {
-        title: themeName + " - DataTables",
-        template: sourcePath + "tb_datatables.ejs",
-        filename: "tb_datatables.html",
-        topmenu: tablesMenu,
-        submenu: "tbdatatables"
+        title:      themeName + " - DataTables",
+        template:   sourcePath + tablesMenu + "_datatables.ejs",
+        filename:   tablesMenu + "_datatables.html",
+        topmenu:    tablesMenu,
+        submenu:    tablesMenu + "datatables"
     }, {
-        title: themeName + " - Form Control",
-        template: sourcePath + "fm_control.ejs",
-        filename: "fm_control.html",
-        topmenu: formsMenu,
-        submenu: "fmcontrol"
+        title:      themeName + " - Form Control",
+        template:   sourcePath + formsMenu + "_control.ejs",
+        filename:   formsMenu + "_control.html",
+        topmenu:    formsMenu,
+        submenu:    formsMenu + "control"
     }, {
-        title: themeName + " - CKEditor Classic",
-        template: sourcePath + "fm_ckeditor_classic.ejs",
-        filename: "fm_ckeditor_classic.html",
-        topmenu: formsMenu,
-        submenu: "fmckeditor",
-        submenu2: 'fmckeditorclassic'
+        title:      themeName + " - CKEditor Classic",
+        template:   sourcePath + formsMenu + "_ckeditor_classic.ejs",
+        filename:   formsMenu + "_ckeditor_classic.html",
+        topmenu:    formsMenu,
+        submenu:    formsMenu + "ckeditor",
+        submenu2:   formsMenu + "ckeditorclassic"
     }, {
-        title: themeName + " - CKEditor Balloon",
-        template: sourcePath + "fm_ckeditor_balloon.ejs",
-        filename: "fm_ckeditor_balloon.html",
-        topmenu: formsMenu,
-        submenu: "fmckeditor",
-        submenu2: 'fmckeditorballoon'
+        title:      themeName + " - CKEditor Balloon",
+        template:   sourcePath + formsMenu + "_ckeditor_balloon.ejs",
+        filename:   formsMenu + "_ckeditor_balloon.html",
+        topmenu:    formsMenu,
+        submenu:    formsMenu + "ckeditor",
+        submenu2:   formsMenu + "ckeditorballoon"
     }, {
-        title: themeName + " - CKEditor Balloon Block",
-        template: sourcePath + "fm_ckeditor_block.ejs",
-        filename: "fm_ckeditor_block.html",
-        topmenu: formsMenu,
-        submenu: "fmckeditor",
-        submenu2: 'fmckeditorblock'
+        title:      themeName + " - CKEditor Balloon Block",
+        template:   sourcePath + formsMenu + "_ckeditor_block.ejs",
+        filename:   formsMenu + "_ckeditor_block.html",
+        topmenu:    formsMenu,
+        submenu:    formsMenu + "ckeditor",
+        submenu2:   formsMenu + "ckeditorblock"
     }, {
-        title: themeName + " - CKEditor Inline",
-        template: sourcePath + "fm_ckeditor_inline.ejs",
-        filename: "fm_ckeditor_inline.html",
-        topmenu: formsMenu,
-        submenu: "fmckeditor",
-        submenu2: 'fmckeditorinline'
+        title:      themeName + " - CKEditor Inline",
+        template:   sourcePath + formsMenu + "_ckeditor_inline.ejs",
+        filename:   formsMenu + "_ckeditor_inline.html",
+        topmenu:    formsMenu,
+        submenu:    formsMenu + "ckeditor",
+        submenu2:   formsMenu + "ckeditorinline"
     }, {
-        title: themeName + " - CKEditor Document",
-        template: sourcePath + "fm_ckeditor_document.ejs",
-        filename: "fm_ckeditor_document.html",
-        topmenu: formsMenu,
-        submenu: "fmckeditor",
-        submenu2: 'fmckeditordocument'
+        title:      themeName + " - CKEditor Document",
+        template:   sourcePath + formsMenu + "_ckeditor_document.ejs",
+        filename:   formsMenu + "_ckeditor_document.html",
+        topmenu:    formsMenu,
+        submenu:    formsMenu + "ckeditor",
+        submenu2:   formsMenu + "ckeditordocument"
     }, {
-        title: themeName + " - Apex Charts",
-        template: sourcePath + "ch_apexcharts.ejs",
-        filename: "ch_apexcharts.html",
-        topmenu: chartsMenu,
-        submenu: "chapex"
+        title:      themeName + " - Apex Charts",
+        template:   sourcePath + chartsMenu + "_apexcharts.ejs",
+        filename:   chartsMenu + "_apexcharts.html",
+        topmenu:    chartsMenu,
+        submenu:    chartsMenu + "apex"
     }, {
-        title: themeName + " - Login",
-        template: sourcePath + "pg_login.ejs",
-        filename: "pg_login.html",
-        topmenu: pagesMenu,
-        submenu: "pglogin"
+        title:      themeName + " - Login",
+        template:   sourcePath + pagesMenu + "_login.ejs",
+        filename:   pagesMenu + "_login.html",
+        topmenu:    pagesMenu,
+        submenu:    pagesMenu + "login"
     }
 ];
